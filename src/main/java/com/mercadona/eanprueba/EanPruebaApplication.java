48a4fe874e4ffc853b7c2f9fedc860a87d0f7ec9
Facu Roly <frr.1991@gmail.com>
2024-07-29 14:15:16 +0200

    Creacion Repositorio

---
+++ src/main/java/com/mercadona/eanprueba/EanPruebaApplication.java
@@ -0,0 +1,13 @@
+package com.mercadona.eanprueba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EanPruebaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EanPruebaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mercadona/eanprueba/EanPruebaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mercadona.eanprueba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EanPruebaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
